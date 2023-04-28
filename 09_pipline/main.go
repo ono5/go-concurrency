8b45f584fb3e64a658ae819ff83323bfbc13b4f5
ono5 <kuroneko_head_lord@yahoo.co.jp>
2023-04-29 06:11:47 +0900

    add pipline code

---
+++ 09_pipline/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+// generator - converts a list of integers to a channel
+func generator(nums ...int) <-chan int {
+	out := make(chan int)
+	go func() {
+		defer close(out)
+		for _, n := range nums {
+			out <- n
+		}
+	}()
+	return out
+}
+
+// square - receive on inbound channel
+// square the number
+// output on outbound channel
+func square(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		defer close(out)
+		for n := range in {
+			out <- n * n
+		}
+	}()
+	return out
+}
+
+func main() {
+	for n := range square(square(generator(1, 2, 3, 4))) {
+		fmt.Println(n)
+	}
+}
---
+++ 09_pipline/template/template.go
@@ -0,0 +1,14 @@
+package main
+
+// generator - converts a list of integers to a channel
+func generator(nums ...int) {
+}
+
+// square - receive on inbound channel
+// square the number
+// output on outbound channel
+func square() {
+}
+
+func main() {
+}
